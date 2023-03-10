36040faa70ed4f3ef903602dcc9fe0679b5d24da
tonyglaz <tonyglazunov@gmail.com>
2023-03-10 19:55:00 +0300

    started creating models

--- ProgSocialNetwork/src/profiles/models.py
+++ ProgSocialNetwork/src/profiles/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Custom user model"""
+    middle_name = models.CharField(max_lenght=50)
+    first_login = models.DateTimeField(null=True)
+    phone = models.CharField(max_length=14)
+    avatar = models.ImageField(upload_to='user/avatar/',blank=True,null=True)
