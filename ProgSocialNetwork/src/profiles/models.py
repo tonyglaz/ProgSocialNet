d6de0e52f09e1009aced8ad28f1bdda9304b7aeb
tonyglaz <tonyglazunov@gmail.com>
2023-03-10 20:26:04 +0300

    .env.dev added

--- ProgSocialNetwork/src/profiles/models.py
+++ ProgSocialNetwork/src/profiles/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class User(AbstractUser):
     """Custom user model"""
-    middle_name = models.CharField(max_lenght=50)
+    middle_name = models.CharField(max_length=50)
     first_login = models.DateTimeField(null=True)
     phone = models.CharField(max_length=14)
     avatar = models.ImageField(upload_to='user/avatar/',blank=True,null=True)
