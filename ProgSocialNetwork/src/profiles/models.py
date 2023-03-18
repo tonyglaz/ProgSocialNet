aa060b4fe3abad676d5e9348b696c74b35079b22
tonyglaz <tonyglazunov@gmail.com>
2023-03-18 14:34:29 +0300

    docker settings finally working

--- ProgSocialNetwork/src/profiles/models.py
+++ ProgSocialNetwork/src/profiles/models.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,AbstractBaseUser
 from django.db import models
 
 
-class User(AbstractUser):
+class User(AbstractBaseUser):
     """Custom user model"""
     middle_name = models.CharField(max_length=50)
     first_login = models.DateTimeField(null=True)
