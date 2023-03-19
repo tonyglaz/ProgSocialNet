990eafb2ca1257b5c8ca723372f18942aff5f7e4
tonyglaz <tonyglazunov@gmail.com>
2023-03-19 23:15:11 +0300

    forgot to add UserAdmin ^

--- ProgSocialNetwork/src/profiles/admin.py
+++ ProgSocialNetwork/src/profiles/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
 from .models import UserNet
 
-admin.site.register(UserNet)
+admin.site.register(UserNet,UserAdmin)
