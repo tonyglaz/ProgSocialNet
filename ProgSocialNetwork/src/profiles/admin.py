a81c8012599bdc9c172011c77ebcb8e0bf342622
tonyglaz <tonyglazunov@gmail.com>
2023-03-20 12:17:02 +0300

    .

--- ProgSocialNetwork/src/profiles/admin.py
+++ ProgSocialNetwork/src/profiles/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
-
-# Register your models here.
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from .models import UserNet
 
-admin.site.register(UserNet,UserAdmin)
+
+class UserNetAdmin(UserAdmin):
+    list_display = ("username", "email", "phone", "first_name", "last_name", "is_staff")
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "middle_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+        (_("Info"), {"fields": ("phone", "avatar")}),
+    )
+
+
+admin.site.register(UserNet, UserNetAdmin)
