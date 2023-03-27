97eb3bf080d11e655a7041f3d22639a0e186beb9
tonyglaz <tonyglazunov@gmail.com>
2023-03-27 11:24:09 +0300

    model of comment added

--- ProgSocialNetwork/src/wall/models.py
+++ ProgSocialNetwork/src/wall/models.py
@@ -1,5 +1,9 @@
-from django.db import models
 from django.conf import settings
+from django.db import models
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
+
+from ..comments.models import AbstractComment
 
 
 class Post(models.Model):
@@ -7,9 +11,25 @@ class Post(models.Model):
     text = models.TextField(max_length=1024)
     create_date = models.DateTimeField(auto_now_add=True)
     published = models.BooleanField(default=True)
-    moderation=models.BooleanField(default=True)
-    view_count=models.PositiveIntegerField(default=0)
-    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    moderation = models.BooleanField(default=True)
+    view_count = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'Post by {self.user}'
+
+
+class Comment(AbstractComment, MPTTModel):
+    """Модель комментариев к постам"""
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    parent = TreeForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='children'
+    )
+
+    def __str__(self):
+        return "{} - {}".format(self.user, self.post)
